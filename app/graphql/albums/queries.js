508dad88ae2182184eb1847a00dc602a1fb46139
Lucas Medrano <lucas.medrano@wolox.com.ar>
2019-07-11 14:26:23 -0300

    Add type-resolver for photos

--- app/graphql/albums/queries.js
+++ app/graphql/albums/queries.js
@@ -1,37 +1,5 @@
 const { gql } = require('apollo-server'),
-  { getAlbumById, requestAlbumPhotos, getAlbums } = require('../../services/typicode'),
-  logger = require('../../logger');
-
-const mapPhotos = photos =>
-  photos.map(photo => ({
-    url: photo.url,
-    thumbnailUrl: photo.thumbnailUrl
-  }));
-
-const getAlbum = albumId =>
-  getAlbumById(albumId).then(album =>
-    requestAlbumPhotos(albumId).then(photos => {
-      album.photos = mapPhotos(photos);
-      return album;
-    })
-  );
-
-const getAllAlbums = (offset, limit, orderedBy) => {
-  logger.info('Requesting albums');
-
-  return getAlbums()
-    .then(foundAlbums => foundAlbums.slice(offset, offset + limit))
-    .then(albumsSliced =>
-      Promise.all(
-        albumsSliced.map(album =>
-          requestAlbumPhotos(album.id).then(photos => {
-            album.photos = mapPhotos(photos);
-          })
-        )
-      ).then(() => albumsSliced)
-    )
-    .then(albums => albums.sort((album1, album2) => (album1[orderedBy] >= album2[orderedBy] ? 1 : -1)));
-};
+  { getAlbum, getAllAlbums } = require('./resolvers');
 
 module.exports = {
   queries: {
---
+++ app/graphql/albums/resolvers.js
@@ -0,0 +1,29 @@
+const { getAlbumById, requestAlbumPhotos, getAlbums } = require('../../services/typicode'),
+  logger = require('../../logger');
+
+exports.getAlbum = albumId =>
+  getAlbumById(albumId).then(album => ({
+    ...album
+  }));
+
+exports.getAllAlbums = (offset, limit, orderedBy) => {
+  logger.info('Requesting albums');
+
+  return getAlbums()
+    .then(foundAlbums => foundAlbums.slice(offset, offset + limit))
+    .then(albumsSliced =>
+      albumsSliced.map(album => ({
+        ...album
+      }))
+    )
+    .then(albums => albums.sort((album1, album2) => (album1[orderedBy] >= album2[orderedBy] ? 1 : -1)));
+};
+
+exports.getPhotos = album => {
+  logger.info(`Requesting photos of album with id ${album.id}`);
+  return requestAlbumPhotos(album.id);
+};
+
+exports.typeResolvers = {
+  photos: exports.getPhotos
+};
