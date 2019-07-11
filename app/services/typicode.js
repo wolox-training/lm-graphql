b0c904f398dd0ec25a71a8274ec21fbdcbc18bfc
Lucas Medrano <lucas.medrano@wolox.com.ar>
2019-07-11 10:31:43 -0300

    Revert "Reduce amount of api calls"
    This reverts commit 349571cfe80c9d355ee8e162bcb4651c12e50829.

--- app/services/typicode.js
+++ app/services/typicode.js
@@ -1,21 +1,13 @@
 const request = require('request-promise'),
   logger = require('../logger'),
   typicodePath = 'https://jsonplaceholder.typicode.com',
-  { apiError } = require('../errors'),
-  lodash = require('lodash');
+  { apiError } = require('../errors');
 
 const options = endpoint => ({
   uri: endpoint,
   json: true,
   resolveWithFullResponse: false
 });
-
-const mapPhotos = (album, photos) => {
-  album.photos = photos.map(photo => ({
-    url: photo.url,
-    thumbnailUrl: photo.thumbnailUrl
-  }));
-};
 
 const requestAlbumPhotos = albumId => {
   logger.info(`Requesting album -with id ${albumId}- photos to jsonplaceholder API`);
@@ -33,7 +25,10 @@ exports.getAlbumById = albumId => {
       return requestAlbumPhotos(albumId);
     })
     .then(photos => {
-      mapPhotos(album, photos);
+      album.photos = photos.map(photo => ({
+        url: photo.url,
+        thumbnailUrl: photo.thumbnailUrl
+      }));
       return album;
     })
     .catch(error => {
@@ -42,18 +37,22 @@ exports.getAlbumById = albumId => {
 };
 
 exports.getAlbums = (offset, limit, orderedBy) => {
-  logger.info('Requesting albums and photos');
+  logger.info('Requesting albums');
   let albums = {};
-  let photos = {};
-  return request(options(`${typicodePath}/photos`))
-    .then(foundPhotos => lodash.groupBy(foundPhotos, 'albumId'))
-    .then(groupedPhotos => {
-      photos = groupedPhotos;
-      return request(options(`${typicodePath}/albums`));
-    })
+
+  return request(options(`${typicodePath}/albums`))
     .then(foundAlbums => {
       albums = foundAlbums;
-      return Promise.all(albums.map(album => mapPhotos(album, photos[album.id])));
+      return Promise.all(
+        albums.map(album =>
+          requestAlbumPhotos(album.id).then(photos => {
+            album.photos = photos.map(photo => ({
+              url: photo.url,
+              thumbnailUrl: photo.thumbnailUrl
+            }));
+          })
+        )
+      );
     })
     .then(() =>
       albums
