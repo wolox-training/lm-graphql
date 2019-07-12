76f475a615a48548d3a5084a949d20966838cf95
Lucas <lucas.medrano@wolox.com.ar>
2019-07-12 12:35:21 -0300

    Email and password verification

---
+++ app/graphql/users/validation.js
@@ -0,0 +1,22 @@
+const yup = require('yup'),
+  { validationError } = require('../../errors'),
+  alphanumericRegex = /^[0-9a-zA-Z]+$/,
+  emailRegex = /.*@wolox.com.ar$/;
+
+const userValidationSchema = yup.object().shape({
+  password: yup
+    .string()
+    .required()
+    .min(8, 'Password should be, at least, 8 characters long')
+    .matches(alphanumericRegex, 'Password should only have alphanumeric characters'),
+  email: yup
+    .string()
+    .required()
+    .email('Invalid email')
+    .matches(emailRegex, 'Email must be from Wolox domain')
+});
+
+exports.validateUser = user =>
+  userValidationSchema.validate(user).catch(error => {
+    throw validationError(error.message);
+  });
