540f7a9bf4a921691deb87c3b7984263b4c27a94
Lucas <lucas.medrano@wolox.com.ar>
2019-07-17 14:57:28 -0300

    Refactor user name test

--- test/users/queries.spec.js
+++ test/users/queries.spec.js
@@ -4,54 +4,44 @@ const { query } = require('../server.spec'),
 
 describe('users', () => {
   describe('queries', () => {
-    it('should get user properly', () =>
-      userFactory.create().then(user =>
-        query(getUser(user.id)).then(res => {
-          expect(res.data).toEqual({
-            user: {
-              firstName: user.firstName,
-              lastName: user.lastName,
-              email: user.email,
-              name: `${user.firstName} ${user.lastName}`
+    describe('single-user', () => {
+      it('should get user properly', () =>
+        userFactory.create().then(user =>
+          query(getUser(user.id)).then(res => {
+            expect(res.data).toEqual({
+              user: {
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+                name: `${user.firstName} ${user.lastName}`
+              }
+            });
+          })
+        ));
+
+      it('should return null when fetching a non existing user', () =>
+        query(getUser(876545678)).then(res => {
+          expect(res.data).toBeNull();
+        }));
+    });
+
+    describe('multi-user', () => {
+      it('should get all users', () =>
+        userFactory.createMany(5).then(() =>
+          query(getUsers()).then(res => {
+            expect(res.data.users).toHaveLength(5);
+            for (let i = 0; i < 5; i++) {
+              expect(res.data.users[i].name).toBe(
+                `${res.data.users[i].firstName} ${res.data.users[i].lastName}`
+              );
             }
-          });
-        })
-      ));
+          })
+        ));
 
-    it('should get all users', () =>
-      userFactory.createMany(5).then(() =>
+      it('should return an empty array wheren there are no users', () =>
         query(getUsers()).then(res => {
-          expect(res.data.users).toHaveLength(5);
-        })
-      ));
-
-    it('should return null when fetching a non existing user', () =>
-      query(getUser(876545678)).then(res => {
-        expect(res.data).toBeNull();
-      }));
-
-    it('should return an empty array wheren there are no users', () =>
-      query(getUsers()).then(res => {
-        expect(res.data.users).toEqual([]);
-      }));
-
-    it("check user's name", () =>
-      userFactory.create().then(user =>
-        query(getUser(user.id)).then(res => {
-          expect(res.data.user.name).toBe(`${res.data.user.firstName} ${res.data.user.lastName}`);
-        })
-      ));
-
-    it('Get users and check their names', () =>
-      userFactory.createMany(5).then(() =>
-        query(getUsers()).then(res => {
-          expect(res.data.users).toHaveLength(5);
-          for (let i = 0; i < 5; i++) {
-            expect(res.data.users[i].name).toBe(
-              `${res.data.users[i].firstName} ${res.data.users[i].lastName}`
-            );
-          }
-        })
-      ));
+          expect(res.data.users).toEqual([]);
+        }));
+    });
   });
 });
