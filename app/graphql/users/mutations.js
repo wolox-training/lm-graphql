e4ead377abdeadf27ae59256e9e316c59ecf7122
Lucas <lucas.medrano@wolox.com.ar>
2019-07-18 11:56:16 -0300

    Refactor validation's code

--- app/graphql/users/mutations.js
+++ app/graphql/users/mutations.js
@@ -7,13 +7,15 @@ module.exports = {
   mutations: {
     createUser: (_, { user }) => validateUser(user).then(() => createUser(user)),
 
-    login: (_, { credentials }) => {
-      userLoggedIn.publish(credentials.email);
-      return {
-        accessToken: signin(credentials)
-      };
-    }
+    login: (_, { credentials }) =>
+      validateUser(credentials).then(() => {
+        userLoggedIn.publish(credentials.email);
+        return {
+          accessToken: signin(credentials)
+        };
+      })
   },
+
   schema: gql`
     extend type Mutation {
       createUser(user: UserInput!): User
